0ad34f4f8886e976fd0e5f834ab8318172e1a196
lluper <leonardoluper5@gmail.com>
2020-06-23 18:48:13 -0300

    erase files will not be used, and create new folders

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './pages/Index/App';
+import './styles/global.css';
+
 
 ReactDOM.render(
   <App />,
