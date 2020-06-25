bf258d8d8da8446d6ac988883c7c73f00be51f48
lluper <leonardoluper5@gmail.com>
2020-06-25 16:52:47 -0300

    CREATE component CategoryBlock

---
+++ src/components/CategoryBlock/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import './index.css';
+
+export default function index() {
+    return (
+        <section className="categoryBlock">
+        <div className="section__header">
+            <h3 className="section__header__title">Categorias populares</h3>
+        </div>
+
+        <div className="categoryBlock__body">
+            <a href="#" className="categoryBlock__card">
+                <span className="categoryBlock__card__icon">icon</span>
+                <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
+            </a>
+
+            <a href="#" className="categoryBlock__card">
+                <span className="categoryBlock__card__icon">icon</span>
+                <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
+            </a>
+
+            <a href="#" className="categoryBlock__card">
+                <span className="categoryBlock__card__icon">icon</span>
+                <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
+            </a>
+
+            <a href="#" className="categoryBlock__card">
+                <span className="categoryBlock__card__icon">icon</span>
+                <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
+            </a>
+
+            <a href="#" className="categoryBlock__card">
+                <span className="categoryBlock__card__icon">icon</span>
+                <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
+            </a>
+
+            <a href="#" className="categoryBlock__card">
+                <span className="categoryBlock__card__icon">icon</span>
+                <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
+            </a>
+
+            <a href="#" className="categoryBlock__card">
+                <span className="categoryBlock__card__icon">icon</span>
+                <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
+            </a>
+
+            <a href="#" className="categoryBlock__card">
+                <span className="categoryBlock__card__icon">icon</span>
+                <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
+            </a>
+        </div>
+    </section>
+
+    )
+}
