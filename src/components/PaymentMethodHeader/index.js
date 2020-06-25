a1c53792dba52396d53a5c5efab8d846ef247f6b
lluper <leonardoluper5@gmail.com>
2020-06-25 16:56:13 -0300

    CREATE component PaymentMethodHeader

---
+++ src/components/PaymentMethodHeader/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './index.css';
+
+export default function index() {
+    return (
+        <div className="paymentMethodsHeader">
+            <div className="paymentMethodsHeader__col col-1">
+                <span className="paymentMethodsHeader__textPrimary">Pagamento rápido e seguro</span>
+                <span className="paymentMethodsHeader__SubText">com Mercado Pago</span>
+            </div>
+            <div className="paymentMethodsHeader__col">
+                <p>icon</p>
+                <div className="paymentMethodsHeader__options">
+                    <span className="paymentMethodsHeader__textPrimary">Pagamento rápido e seguro</span>
+                    <span className="paymentMethodsHeader__SubText">com Mercado Pago</span>
+                </div>
+            </div>
+            <div className="paymentMethodsHeader__col">
+                <p>icon</p>
+                <div className="paymentMethodsHeader__options">
+                    <span className="paymentMethodsHeader__textPrimary">Pagamento rápido e seguro</span>
+                    <span className="paymentMethodsHeader__SubText">com Mercado Pago</span>
+                </div>
+            </div>
+            <div className="paymentMethodsHeader__col">
+                <p>icon</p>
+                <div className="paymentMethodsHeader__options">
+                    <span className="paymentMethodsHeader__textPrimary">Pagamento rápido e seguro</span>
+                    <span className="paymentMethodsHeader__SubText">com Mercado Pago</span>
+                </div>
+            </div>
+        </div>
+
+    )
+}
