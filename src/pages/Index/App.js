c76ade3f46320aa63396e4e2cfb7552e74f87648
lluper <leonardoluper5@gmail.com>
2020-06-25 16:59:44 -0300

    MODIFIED page Index

--- src/pages/Index/App.js
+++ src/pages/Index/App.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import NavBar from '../../components/NavBar/index';
+import NavBarMobile from '../../components/NavBarMobile';
+import Slider from '../../components/Slider';
+import PaymentMethodHeader from '../../components/PaymentMethodHeader';
+import OffersBlock from '../../components/OffersBlock';
+import DiscoverBlock from '../../components/DiscoverBlock';
+import BestStoresBlock from '../../components/BestStoresBlock';
+import CategoryBlock from '../../components/CategoryBlock';
+import HistoryBlock from '../../components/HistoryBlock';
+import Footer from '../../components/Footer';
+
 import './App.css';
+
+
+
 
 function App() {
     return (
-        <div>
-            <NavBar />
-            <h1>ola mundo</h1>
-        </div>
+        <React.Fragment>
+            <header>
+                <NavBar />
+                <NavBarMobile />
+                <Slider />
+            </header>
+
+            <main>
+                <PaymentMethodHeader />
+                <OffersBlock />
+                <DiscoverBlock />
+                <BestStoresBlock />
+                <CategoryBlock />
+                <HistoryBlock />
+            </main>
+            <Footer />
+
+        </React.Fragment>
+
+
     );
 }
 
