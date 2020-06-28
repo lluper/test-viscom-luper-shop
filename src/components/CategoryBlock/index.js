9e9dfc748231beacb612ea21389bcddbc3e9d24d
lluper <leonardoluper5@gmail.com>
2020-06-28 17:14:26 -0300

    MODIFIED components , custom css and struct

--- src/components/CategoryBlock/index.js
+++ src/components/CategoryBlock/index.js
@@ -1,55 +1,87 @@
 import React from 'react'
 import './index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { 
+    faCoffee,
+    faCarSide,
+    faKeyboard,
+    faMobileAlt,
+    faMouse,
+    faLaptop,
+    faDesktop,
+    faServer,
+    faMicrochip
+
+ } from '@fortawesome/free-solid-svg-icons'
+
+import {Link} from 'react-router-dom'
+
 
 export default function index() {
     return (
         <section className="categoryBlock">
-        <div className="section__header">
-            <h3 className="section__header__title">Categorias populares</h3>
-        </div>
+            <div className="section__header">
+                <h3 className="section__header__title">Categorias populares</h3>
+            </div>
 
-        <div className="categoryBlock__body">
-            <a href="#" className="categoryBlock__card">
-                <span className="categoryBlock__card__icon">icon</span>
-                <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
-            </a>
+            <div className="categoryBlock__body">
+                <Link to="/search" className="categoryBlock__card">
+                    <span className="categoryBlock__card__icon">
+                        <FontAwesomeIcon icon={faCarSide} className="categoryBlock__icon" />
+                    </span>
+                    <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
+                </Link>
 
-            <a href="#" className="categoryBlock__card">
-                <span className="categoryBlock__card__icon">icon</span>
-                <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
-            </a>
+                <Link to="/search" className="categoryBlock__card">
+                    <span className="categoryBlock__card__icon">
+                        <FontAwesomeIcon icon={faKeyboard} className="categoryBlock__icon" />
+                    </span>
+                    <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
+                </Link>
 
-            <a href="#" className="categoryBlock__card">
-                <span className="categoryBlock__card__icon">icon</span>
-                <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
-            </a>
+                <Link to="/search" className="categoryBlock__card">
+                    <span className="categoryBlock__card__icon">
+                        <FontAwesomeIcon icon={faMobileAlt} className="categoryBlock__icon" />
+                    </span>
+                    <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
+                </Link>
 
-            <a href="#" className="categoryBlock__card">
-                <span className="categoryBlock__card__icon">icon</span>
-                <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
-            </a>
+                <Link to="/search" className="categoryBlock__card">
+                    <span className="categoryBlock__card__icon">
+                        <FontAwesomeIcon icon={faMouse} className="categoryBlock__icon" />
+                    </span>
+                    <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
+                </Link>
 
-            <a href="#" className="categoryBlock__card">
-                <span className="categoryBlock__card__icon">icon</span>
-                <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
-            </a>
+                <Link to="/search" className="categoryBlock__card">
+                    <span className="categoryBlock__card__icon">
+                        <FontAwesomeIcon icon={faLaptop} className="categoryBlock__icon" />
+                    </span>
+                    <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
+                </Link>
 
-            <a href="#" className="categoryBlock__card">
-                <span className="categoryBlock__card__icon">icon</span>
-                <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
-            </a>
+                <Link to="/search" className="categoryBlock__card">
+                    <span className="categoryBlock__card__icon">
+                        <FontAwesomeIcon icon={faDesktop} className="categoryBlock__icon" />
+                    </span>
+                    <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
+                </Link>
 
-            <a href="#" className="categoryBlock__card">
-                <span className="categoryBlock__card__icon">icon</span>
-                <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
-            </a>
+                <Link to="/search" className="categoryBlock__card">
+                    <span className="categoryBlock__card__icon">
+                        <FontAwesomeIcon icon={faServer} className="categoryBlock__icon" />
+                    </span>
+                    <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
+                </Link>
 
-            <a href="#" className="categoryBlock__card">
-                <span className="categoryBlock__card__icon">icon</span>
-                <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
-            </a>
-        </div>
-    </section>
+                <Link to="/search" className="categoryBlock__card">
+                    <span className="categoryBlock__card__icon">
+                        <FontAwesomeIcon icon={faMicrochip} className="categoryBlock__icon" />
+                    </span>
+                    <h3 className="categoryBlock__card__title">Carros, motos e outro</h3>
+                </Link>
+            </div>
+        </section>
 
     )
 }
