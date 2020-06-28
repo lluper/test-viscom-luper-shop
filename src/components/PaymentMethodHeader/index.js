72118980a88b7d0251391659a67687b10479f6f0
lluper <leonardoluper5@gmail.com>
2020-06-28 17:16:00 -0300

    MODIFIED components , custom css and struct

--- src/components/PaymentMethodHeader/index.js
+++ src/components/PaymentMethodHeader/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlusCircle, faHandHoldingUsd } from '@fortawesome/free-solid-svg-icons'
+import { faCreditCard } from '@fortawesome/free-regular-svg-icons'
+
+
 
 export default function index() {
     return (
@@ -9,24 +14,24 @@ export default function index() {
                 <span className="paymentMethodsHeader__SubText">com Mercado Pago</span>
             </div>
             <div className="paymentMethodsHeader__col">
-                <p>icon</p>
+                <FontAwesomeIcon icon={faCreditCard} className="paymentMethodsHeader__icon" />
                 <div className="paymentMethodsHeader__options">
-                    <span className="paymentMethodsHeader__textPrimary">Pagamento rápido e seguro</span>
-                    <span className="paymentMethodsHeader__SubText">com Mercado Pago</span>
+                    <span className="paymentMethodsHeader__textPrimary">Até 12 vezes sem juros</span>
+                    <span className="paymentMethodsHeader__link">ver mais</span>
                 </div>
             </div>
             <div className="paymentMethodsHeader__col">
-                <p>icon</p>
+                <FontAwesomeIcon icon={faHandHoldingUsd} className="paymentMethodsHeader__icon" />
                 <div className="paymentMethodsHeader__options">
-                    <span className="paymentMethodsHeader__textPrimary">Pagamento rápido e seguro</span>
-                    <span className="paymentMethodsHeader__SubText">com Mercado Pago</span>
+                    <span className="paymentMethodsHeader__textPrimary">À vista no boleto bancário</span>
+                    <span className="paymentMethodsHeader__link">ver mais</span>
                 </div>
             </div>
             <div className="paymentMethodsHeader__col">
-                <p>icon</p>
+                <FontAwesomeIcon icon={faPlusCircle} className="paymentMethodsHeader__icon" />
                 <div className="paymentMethodsHeader__options">
-                    <span className="paymentMethodsHeader__textPrimary">Pagamento rápido e seguro</span>
-                    <span className="paymentMethodsHeader__SubText">com Mercado Pago</span>
+                    <span className="paymentMethodsHeader__textPrimary">Mais formas de pagamento</span>
+                    <span className="paymentMethodsHeader__link">ver mais</span>
                 </div>
             </div>
         </div>
